f4decad15347943267f53f0e09691c697e2d1d3d
godarcher <joostgrunwald2001@gmail.com>
2021-10-11 14:54:36 +0200

    Co-authored-by: martijn.van.de.wouw@hotmail.com <martijn.van.de.wouw@hotmail.com>

--- project/main.cpp
+++ project/main.cpp
@@ -8,43 +8,53 @@
   Hint: Ctrl-space to auto complete a functionname/variable.
   */
 
-// function/class definitions you are going to use
+//dependencies
 #include <algorithm>
 #include <iostream>
 #include <mutex>
 #include <thread>
 #include <vector>
-#include <string> //added by us
+//includes from here one are our own
+#include <string>
 
-// although it is good habit, you don't have to type 'std::' before many objects by including this line
 using namespace std;
 
-// for now we declare the important stuff here
-//TODO decide where this stays here
+// ! Global variables
 vector<int> buffer;
 vector<string> logger;
 bool bounded = false;
 int bufferbound = 0;
 
-//TODO We do all synchronization of these functions later on (happens in general function not in these ones)
+// * This function writes a single log to the log vector
 void writeToLog(string log)
 {
   logger.push_back(log);
 }
 
+// * This function reads a specific log sample, by using an index as input
 string readFromLog(int index)
 {
   return logger[index];
 }
 
-//TODO We do all synchronization of these functions later on (happens in general function not in these ones)
+// * This function prints the entire log onto the terminal
+void printLog()
+{
+  for (string element : logger)
+    std::cout << element << ' ';
+  std::cout << endl;
+}
+
+// * This function writes to the buffer, it therefore
+// * Checks wether or not it is already full if bounden
+// * the function also updates the log with operation about its success
 void writeToBuffer(int element)
 {
   if (bounded == true)
   {
     if (buffer.size() < bufferbound)
     {
-      // * There is room left so we push
+      // ? There is room left so we push
       buffer.push_back(element);
       writeToLog("operation succeeded, added: " + to_string(element) + " to buffer");
     }
@@ -53,13 +63,16 @@ void writeToBuffer(int element)
       writeToLog("operation failed: The buffer has already reached its bound");
     }
   }
-  else //not bounded
+  else // ? not bounded
   {
     buffer.push_back(element);
     writeToLog("operation succeeded, added: " + to_string(element) + " to buffer");
   }
 }
 
+// * This function sets the max bound of the buffer
+// * as base case we check for legitimate inputs
+// * It also provides the log with information about its success
 void setBufferBound(int userbound)
 {
   if (userbound == 0)
@@ -89,13 +102,6 @@ void removeBufferBound()
 void printBuffer()
 {
   for (int element : buffer)
-    std::cout << element << ' ';
-  std::cout << endl;
-}
-
-void printLogger()
-{
-  for (int element : logger)
     std::cout << element << ' ';
   std::cout << endl;
 }
