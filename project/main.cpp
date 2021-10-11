ad23f199e5a3c9ee30a3ab6adb21a6ccb16ee389
godarcher <joostgrunwald2001@gmail.com>
2021-10-11 12:33:45 +0200

    added all needed stuff

---
+++ project/main.cpp
@@ -0,0 +1,24 @@
+/**
+  * Assignment: synchronization
+  * Operating Systems
+  */
+
+/**
+  Hint: F2 (or Control-klik) on a functionname to jump to the definition
+  Hint: Ctrl-space to auto complete a functionname/variable.
+  */
+
+// function/class definitions you are going to use
+#include <algorithm>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+// although it is good habit, you don't have to type 'std::' before many objects by including this line
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+	return 0;
+}
